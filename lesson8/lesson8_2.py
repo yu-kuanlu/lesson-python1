980f6531ec7527c38b48bb26b104c8b8a319c264
yu-kuanlu <damenmijikou@gmail.com>
2025-11-11 03:26:52 +0000

    25/11/10

---
+++ lesson8/lesson8_2.py
@@ -0,0 +1,11 @@
+import os
+import random
+
+def get_names(file_name):""
+
+def get_scores(names, num=10):""
+
+names = get_names("names.txt")
+num = int(input("輸入數量:"))
+students = get_scores(names,num=num)
+print(students)
