ab33f9ca176671af8496b5098a604db627ad6779
yu-kuanlu <damenmijikou@gmail.com>
2025-11-11 04:29:52 +0000

    25/11/11_2

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -1,11 +1,15 @@
 import os
 import random
+import csv
 
-def get_names(file_name):""
 
-def get_scores(names, num=10):""
-
+def get_names(file_name):
+    
+def get_scores(names, num=10):
+    
+def save_csv(students,filename):
+    
 names = get_names("names.txt")
 num = int(input("輸入數量:"))
 students = get_scores(names,num=num)
-print(students)
+save_csv(students,'students.csv')
